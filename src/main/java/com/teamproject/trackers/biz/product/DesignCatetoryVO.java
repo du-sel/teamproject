9076cb4988128e3beaa703a4182f1a2bb37ed36b
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-11 17:22:53 +0900

    DesignCatetoryVO, PageCatetoryVO

---
+++ src/main/java/com/teamproject/trackers/biz/product/DesignCatetoryVO.java
@@ -0,0 +1,35 @@
+package com.teamproject.trackers.vo;
+
+public class DesignCatetoryVO {
+	private int p_id;
+	private boolean minimal;
+	private boolean illustration;
+	private boolean photo;
+	
+	
+	public int getP_id() {
+		return p_id;
+	}
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+	public boolean isMinimal() {
+		return minimal;
+	}
+	public void setMinimal(boolean minimal) {
+		this.minimal = minimal;
+	}
+	public boolean isIllustration() {
+		return illustration;
+	}
+	public void setIllustration(boolean illustration) {
+		this.illustration = illustration;
+	}
+	public boolean isPhoto() {
+		return photo;
+	}
+	public void setPhoto(boolean photo) {
+		this.photo = photo;
+	}
+	
+}
---
+++ src/main/java/com/teamproject/trackers/biz/product/PageCatetoryVO.java
@@ -0,0 +1,84 @@
+package com.teamproject.trackers.vo;
+
+public class PageCatetoryVO {
+	private int p_id;
+	private boolean set;
+	private boolean monthly;
+	private boolean weekly;
+	private boolean habit;
+	private boolean mood;
+	private boolean reading;
+	private boolean account_book;
+	private boolean study;
+	private boolean sticker;
+	private boolean etc;
+	
+	
+	public int getP_id() {
+		return p_id;
+	}
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+	public boolean isSet() {
+		return set;
+	}
+	public void setSet(boolean set) {
+		this.set = set;
+	}
+	public boolean isMonthly() {
+		return monthly;
+	}
+	public void setMonthly(boolean monthly) {
+		this.monthly = monthly;
+	}
+	public boolean isWeekly() {
+		return weekly;
+	}
+	public void setWeekly(boolean weekly) {
+		this.weekly = weekly;
+	}
+	public boolean isHabit() {
+		return habit;
+	}
+	public void setHabit(boolean habit) {
+		this.habit = habit;
+	}
+	public boolean isMood() {
+		return mood;
+	}
+	public void setMood(boolean mood) {
+		this.mood = mood;
+	}
+	public boolean isReading() {
+		return reading;
+	}
+	public void setReading(boolean reading) {
+		this.reading = reading;
+	}
+	public boolean isAccount_book() {
+		return account_book;
+	}
+	public void setAccount_book(boolean account_book) {
+		this.account_book = account_book;
+	}
+	public boolean isStudy() {
+		return study;
+	}
+	public void setStudy(boolean study) {
+		this.study = study;
+	}
+	public boolean isSticker() {
+		return sticker;
+	}
+	public void setSticker(boolean sticker) {
+		this.sticker = sticker;
+	}
+	public boolean isEtc() {
+		return etc;
+	}
+	public void setEtc(boolean etc) {
+		this.etc = etc;
+	}
+	
+}
