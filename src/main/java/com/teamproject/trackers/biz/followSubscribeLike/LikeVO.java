99de559b1db1341a22b69c1b469a7dafaaf6145f
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-11 17:23:08 +0900

    LikeVO

---
+++ src/main/java/com/teamproject/trackers/biz/followSubscribeLike/LikeVO.java
@@ -0,0 +1,37 @@
+package com.teamproject.trackers.vo;
+
+import java.sql.Date;
+
+public class LikeVO {
+	private int post_id;
+	private int id;
+	private String content;
+	private Date cre_date;
+	
+	
+	public int getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getCre_date() {
+		return cre_date;
+	}
+	public void setCre_date(Date cre_date) {
+		this.cre_date = cre_date;
+	}
+	
+}
