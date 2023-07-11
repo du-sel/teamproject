d27022bee3b1e812130ac7cc0f0b8202719c8415
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-11 17:22:23 +0900

    UserVO, CreatorVO

--- src/main/java/com/teamproject/trackers/biz/userCreator/CreatorVO.java
+++ src/main/java/com/teamproject/trackers/biz/userCreator/CreatorVO.java
@@ -1,5 +1,41 @@
-package com.teamproject.trackers.biz.userCreator;
+package com.teamproject.trackers.vo;
 
 public class CreatorVO {
-
+	private int id;
+	private String store_name;
+	private String bank;
+	private String account;
+	private boolean subscribe;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStore_name() {
+		return store_name;
+	}
+	public void setStore_name(String store_name) {
+		this.store_name = store_name;
+	}
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public boolean isSubscribe() {
+		return subscribe;
+	}
+	public void setSubscribe(boolean subscribe) {
+		this.subscribe = subscribe;
+	}
 }
--- src/main/java/com/teamproject/trackers/biz/userCreator/UserVO.java
+++ src/main/java/com/teamproject/trackers/biz/userCreator/UserVO.java
@@ -1,5 +1,69 @@
-package com.teamproject.trackers.biz.userCreator;
+package com.teamproject.trackers.vo;
 
 public class UserVO {
-
+	private int id;
+	private String email;
+	private String password;
+	private String name;
+	private String tel;
+	private String instagram;
+	private String youtube;
+	private String profile_img;
+	private String bg_img;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getInstagram() {
+		return instagram;
+	}
+	public void setInstagram(String instagram) {
+		this.instagram = instagram;
+	}
+	public String getYoutube() {
+		return youtube;
+	}
+	public void setYoutube(String youtube) {
+		this.youtube = youtube;
+	}
+	public String getProfile_img() {
+		return profile_img;
+	}
+	public void setProfile_img(String profile_img) {
+		this.profile_img = profile_img;
+	}
+	public String getBg_img() {
+		return bg_img;
+	}
+	public void setBg_img(String bg_img) {
+		this.bg_img = bg_img;
+	}
 }
