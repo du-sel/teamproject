316aa9eb5b7aac353c956a310a43b49ef353785e
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-11 17:19:31 +0900

    PostVO, PostIMGVO

--- src/main/java/com/teamproject/trackers/biz/post/PostVO.java
+++ src/main/java/com/teamproject/trackers/biz/post/PostVO.java
@@ -1,5 +1,44 @@
-package com.teamproject.trackers.biz.post;
+package com.teamproject.trackers.vo;
+
+import java.sql.Date;
 
 public class PostVO {
-
+	private int post_id;
+	private int id;
+	private int p_id;
+	private Date cre_date;
+	private String content;
+	
+	
+	public int getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getP_id() {
+		return p_id;
+	}
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+	public Date getCre_date() {
+		return cre_date;
+	}
+	public void setCre_date(Date cre_date) {
+		this.cre_date = cre_date;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
 }
