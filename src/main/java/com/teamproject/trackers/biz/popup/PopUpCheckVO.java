6a0fc5d8136a46b9c48a0c5b0ee1a2fa4b7b5ecc
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-11 17:19:10 +0900

    PopUpVO, PopUpCheckVO

---
+++ src/main/java/com/teamproject/trackers/biz/popup/PopUpCheckVO.java
@@ -0,0 +1,27 @@
+package com.teamproject.trackers.vo;
+
+public class PopUpCheckVO {
+	private int pop_id;
+	private int id;
+	private boolean checked;
+	
+	
+	public int getPop_id() {
+		return pop_id;
+	}
+	public void setPop_id(int pop_id) {
+		this.pop_id = pop_id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean isChecked() {
+		return checked;
+	}
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+}
