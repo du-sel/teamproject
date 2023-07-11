10beceaa9e2a24364a5791156bd2242b283eb4f1
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-11 17:21:55 +0900

    ReviewVO, InquiryVO

--- src/main/java/com/teamproject/trackers/biz/reviewInquiry/InquiryVO.java
+++ src/main/java/com/teamproject/trackers/biz/reviewInquiry/InquiryVO.java
@@ -1,5 +1,58 @@
-package com.teamproject.trackers.biz.reviewInquiry;
+package com.teamproject.trackers.vo;
+
+import java.sql.Date;
 
 public class InquiryVO {
-
+	private int inquiry_id;
+	private int p_id;
+	private int id;
+	private Date cre_date;
+	private String title;
+	private String content;
+	private String answer;
+	
+	
+	public int getInquiry_id() {
+		return inquiry_id;
+	}
+	public void setInquiry_id(int inquiry_id) {
+		this.inquiry_id = inquiry_id;
+	}
+	public int getP_id() {
+		return p_id;
+	}
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getCre_date() {
+		return cre_date;
+	}
+	public void setCre_date(Date cre_date) {
+		this.cre_date = cre_date;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
 }
