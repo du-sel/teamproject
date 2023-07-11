e43dfb0890bc9c84094beb7e74e8d904dfd14760
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-11 17:21:28 +0900

    PurchaseVO

--- src/main/java/com/teamproject/trackers/biz/purchase/PurchaseVO.java
+++ src/main/java/com/teamproject/trackers/biz/purchase/PurchaseVO.java
@@ -1,5 +1,36 @@
-package com.teamproject.trackers.biz.purchase;
+package com.teamproject.trackers.vo;
+
+import java.sql.Date;
 
 public class PurchaseVO {
-
+	private int id;
+	private int p_id;
+	private Date cre_date;
+	private int price;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getP_id() {
+		return p_id;
+	}
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+	public Date getCre_date() {
+		return cre_date;
+	}
+	public void setCre_date(Date cre_date) {
+		this.cre_date = cre_date;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
