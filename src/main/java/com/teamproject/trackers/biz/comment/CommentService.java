cf75c368fdac162cdff20ee4053ce72399888517
hje00928 <136958945+hje00928@users.noreply.github.com>
2023-07-16 11:33:11 +0900

    Add files via upload

---
+++ src/main/java/com/teamproject/trackers/biz/comment/CommentService.java
@@ -0,0 +1,30 @@
+package com.teamproject.trackers.biz.comment;
+ 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class CommentService {
+
+	@Autowired
+	@Qualifier("commentRepository")
+	private CommentRepository commentRepository;
+	
+	// 작성
+	public void insertComment(CommentVO vo) {
+		commentRepository.save(vo);
+	}
+	
+	// 삭제
+	public void deleteComment(Long comment_id) {
+		commentRepository.deleteById(comment_id);
+	}
+	
+	// 리스트 조회
+	public List<CommentVO> getCommentList(){
+		return commentRepository.findAll();
+	}
+}
