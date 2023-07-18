2b2debb335653c1a6d531ec71f8d2459fa4507b8
du-sel <ymdlou@gmail.com>
2023-07-18 19:53:00 +0900

    오류수정

--- src/main/java/com/teamproject/trackers/biz/cart/CartId.java
+++ src/main/java/com/teamproject/trackers/biz/cart/CartId.java
@@ -1,8 +1,10 @@
 package com.teamproject.trackers.biz.cart;
 
+import java.io.Serializable;
+
 import javax.persistence.Id;
 
-public class CartId {
+public class CartId implements Serializable {
 
 	private long id;
 	private long p_id;
