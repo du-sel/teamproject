1575540f3ad4ef0ca2936ff8c2ebbcbe7dca3fcb
hje00928 <136958945+hje00928@users.noreply.github.com>
2023-07-20 01:37:47 +0900

    Update CommentService.java

--- src/main/java/com/teamproject/trackers/biz/comment/CommentService.java
+++ src/main/java/com/teamproject/trackers/biz/comment/CommentService.java
@@ -1,10 +1,15 @@
 package com.teamproject.trackers.biz.comment;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import com.teamproject.trackers.biz.userCreator.UserVO;
 
 @Service 
 public class CommentService {
@@ -13,18 +18,32 @@ public class CommentService {
 	@Qualifier("commentRepository")
 	private CommentRepository commentRepository;
 	
+	
 	// 작성
 	public void insertComment(CommentVO vo) {
-		commentRepository.save(vo);
+System.out.println("vopoid "+vo.getPostId());		
+System.out.println("void "+vo.getId());		
+System.out.println("voco "+vo.getContent());		
+		commentRepository.save(vo.getPostId(), vo.getId(), vo.getContent());
 	}
 	
 	// 삭제
-	public void deleteComment(Long comment_id) {
-		commentRepository.deleteById(comment_id);
+	public void deleteComment(Long commentid) {
+		commentRepository.deleteByCommentid(commentid);
 	}
 	
 	// 리스트 조회
-	public List<CommentVO> getCommentList(){
-		return commentRepository.findAll();
+	public ArrayList<CommentVO> getCommentList(Long postId){
+System.out.println("colist postid "+postId);	
+		return (ArrayList)commentRepository.findAllByPostId(postId);
 	}
+	
+	public Optional<UserVO> getUser(Long commentId) {
+        Optional<CommentVO> commentOptional = commentRepository.findById(commentId);
+        if (commentOptional.isPresent()) {
+        	CommentVO commentVO = commentOptional.get();
+            return commentRepository.findUserByUserId(commentVO.getId());
+        }
+        return Optional.empty();
+    }
 }
