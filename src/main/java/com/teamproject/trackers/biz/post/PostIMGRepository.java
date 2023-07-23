14c62f65e56b2960ac9e8878f1253470a454b2c9
hje00928 <136958945+hje00928@users.noreply.github.com>
2023-07-24 00:00:49 +0900

    Update PostIMGRepository.java

--- src/main/java/com/teamproject/trackers/biz/post/PostIMGRepository.java
+++ src/main/java/com/teamproject/trackers/biz/post/PostIMGRepository.java
@@ -16,9 +16,9 @@ public interface PostIMGRepository extends JpaRepository<PostIMGVO, Long>{
 	// 작성
 		public PostIMGVO save(PostIMGVO vo); 
 	// 삭제
-		public void deleteByPostimg(Long postId);
+		public void deleteByPostId(Long postId);
 	// 상세 조회
-		public Optional<PostIMGVO> findByPostimg(Long postId);
+		public Optional<PostIMGVO> findByPostId(Long postId);
 	// 리스트 조회
 		public List<PostIMGVO> findAll();
 
