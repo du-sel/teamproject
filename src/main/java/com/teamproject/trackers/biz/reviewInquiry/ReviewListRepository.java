166a0618a1cacdec223083385f1a3ec9b4bc2f82
jaeeun25 <124561558+jaeeun25@users.noreply.github.com>
2023-07-25 18:10:33 +0900

    판매자 리뷰 리스트

--- src/main/java/com/teamproject/trackers/biz/reviewInquiry/ReviewListRepository.java
+++ src/main/java/com/teamproject/trackers/biz/reviewInquiry/ReviewListRepository.java
@@ -2,12 +2,21 @@ package com.teamproject.trackers.biz.reviewInquiry;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.teamproject.trackers.biz.purchase.PurchaseListVO;
+
 
 public interface ReviewListRepository extends JpaRepository<ReviewListVO, ReviewId> {
-	
+
+	// 상품별 리뷰 리스트
 	List<ReviewListVO> findAllByPidOrderByCreDateDesc(long pid);
+
+	// 크리에이터별 리뷰 리스트
+	Page<ReviewListVO> findAllByCid(long cid, Pageable pageable);
+
 }
